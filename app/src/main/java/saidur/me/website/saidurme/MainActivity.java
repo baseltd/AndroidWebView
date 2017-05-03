4742307988fe9a8116a3016397b4676f774d8490
saidurcse <saidursd@gmail.com>
2017-05-03 23:59:56 +0600

    first commit

---
+++ app/src/main/java/saidur/me/website/saidurme/MainActivity.java
@@ -0,0 +1,141 @@
+package saidur.me.website.saidurme;
+
+import android.app.PendingIntent;
+import android.app.TaskStackBuilder;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+// Website mube be responsive website
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar progressBar;
+    private LinearLayout layoutProgress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Custom title bar
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        layoutProgress = (LinearLayout) findViewById(R.id.layoutProgress);
+        webView.setVisibility(View.GONE);
+        WebSettings settings = webView.getSettings();
+
+        //Using JavaScript in WebView, it will enable Javascript support for your website
+        settings.setJavaScriptEnabled(true);
+
+        //Zoom control
+        settings.setBuiltInZoomControls(true);
+        settings.setSupportZoom(true);
+        settings.setDisplayZoomControls(false);
+
+        //Handling Page Navigation
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.setVisibility(View.VISIBLE);
+                layoutProgress.setVisibility(View.GONE);
+                progressBar.setIndeterminate(false);
+                super.onPageFinished(view, url);
+
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                layoutProgress.setVisibility(View.VISIBLE);
+                progressBar.setIndeterminate(true);
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+        if(isOnline()) {
+            webView.loadUrl("http://google.com/");
+        } else {
+            String summary = "<html><body><font color='red'>No Internet Connection</font></body></html>";
+            webView.loadData(summary, "text/html", null);
+            toast("No Internet Connection.");
+        }
+    }
+    private void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+    }
+
+    //Navigating web page history
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+    // Must must we need to override onPause() otherwise you app will reject from google play store
+    @Override
+    public void onPause() {
+        super.onPause();
+        webView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        webView.onResume();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
---
+++ app/src/main/java/saidur/me/website/saidurme/Test.java
@@ -0,0 +1,96 @@
+package saidur.me.website.saidurme;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.LinearLayout;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+// Webview
+// Progress
+// Back key implementaion
+// Margin correction
+// Webapge should open in the app only
+// If Internet connection is not present
+// Zoom
+
+public class Test extends AppCompatActivity {
+    private WebView webView;
+    private ProgressBar progressBar;
+    private LinearLayout layoutProgress;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webView);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+        layoutProgress = (LinearLayout) findViewById(R.id.layoutProgress);
+        webView.setVisibility(View.GONE);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setBuiltInZoomControls(true);
+        settings.setSupportZoom(true);
+        settings.setDisplayZoomControls(false);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                webView.setVisibility(View.VISIBLE);
+                layoutProgress.setVisibility(View.GONE);
+                progressBar.setIndeterminate(false);
+                super.onPageFinished(view, url);
+
+            }
+
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                layoutProgress.setVisibility(View.VISIBLE);
+                progressBar.setIndeterminate(true);
+                super.onPageStarted(view, url, favicon);
+            }
+        });
+        if(isOnline()) {
+            webView.loadUrl("http://saidur.me/");
+        } else {
+            String summary = "<html><body><font color='red'>No Internet Connection</font></body></html>";
+            webView.loadData(summary, "text/html", null);
+            toast("No Internet Connection.");
+        }
+    }
+    private void toast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    private boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return (netInfo != null && netInfo.isConnected());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && webView.canGoBack()) {
+            webView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
+    }
+
+}
